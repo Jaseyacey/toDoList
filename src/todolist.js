5a665c17a33b1e74ec0452369eba6ba023c800ef
Jason Beedle <jason_beedle@hotmail.com>
2020-04-19 18:15:52 +0100

    adding add task

--- src/todolist.js
+++ src/todolist.js
@@ -1,17 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./ToDoList.css";
+import ToDoItems from "./ToDoItems";
 
 class ToDoList extends Component {
-    render() {
-        return (
-            <div className="ToDoListMain">
-                <div className ="header">
-                    <form>
-                        <input placeholder="Enter Task" />
-                        <button type = "submit"> Add </button>
-                    </form>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);        
+    }
+
+    addItem(e) {
+        var itemArray = this.state.items; 
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift (
+                {
+                    text: this._inputElement.value,
+                    key: Date.now()
+                }
+            );
+            this.setState({
+                items: itemArray
+            });
+        }
+
+        render() {
+            return(
+                <div className="toDoListMain">
+                    <div className="header">
+                        <form onSubmit = {this.addItem}>
+                            input ref={(a) => this._inputElement = a}
+                            placeholder="enter task"
+                        </form>
+                    </div>
+
+                <ToDoItems entries={this.state.items}
+                    delete={this.deleteItem} />
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
